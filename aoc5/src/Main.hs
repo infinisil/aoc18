afd7dfb49b32555721086d0071a3e5dcd4f94684
Silvan Mosberger <infinisil@icloud.com>
2018-12-05 20:00:44 +0100

    aoc5: Improve part2, add timing

--- aoc5/src/Main.hs
+++ aoc5/src/Main.hs
@@ -6,6 +6,7 @@ import           Data.Char
 import           Data.List
 import           Data.Text                       (Text)
 import qualified Data.Text.IO                    as TIO
+import           Data.Time.Clock
 import           Data.Void
 import           Paths_aoc5
 import           Statistics.Distribution.Poisson
@@ -28,21 +29,31 @@ main = do
   input <- head . lines <$> readFile inputFile
   challenges input
 
+time :: IO a -> IO a
+time io = do
+  start <- getCurrentTime
+  result <- io
+  stop <- getCurrentTime
+  print $ stop `diffUTCTime` start
+  return result
+
 challenges :: Input -> IO ()
 challenges input = do
-  print $ part1 input
-  print $ part2 input
+  time $ print $ part1 input
+  time $ print $ part2 input
   return ()
 
-react :: [Char] -> [Char] -> [Char]
-react stack []      = stack
-react [] (c:cs)     = react [c] cs
-react (x:xs) (c:cs)
-  | toLower x == toLower c && x /= c = react xs cs
-  | otherwise = react (c:x:xs) cs
+react :: String -> String
+react = go [] where
+  go stack []      = stack
+  go [] (c:cs)     = go [c] cs
+  go (x:xs) (c:cs)
+    | toLower x == toLower c && x /= c = go xs cs
+    | otherwise = go (c:x:xs) cs
 
 part1 :: Input -> Int
-part1 input = length $ react [] input
+part1 input = length $ react input
 
 part2 :: Input -> Int
-part2 input = minimum $ map (\c -> length $ react "" $ filter ((/=c) . toLower) input) ['a'..'z']
+part2 input = minimum $ map (\c -> length $ react $ filter ((/=c) . toLower) input') ['a'..'z'] where
+  input' = react input
