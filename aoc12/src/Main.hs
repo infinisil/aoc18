efc14b3f75b073e0d6f1dcc95fdadae319af4595
Silvan Mosberger <infinisil@icloud.com>
2018-12-13 01:59:25 +0100

    aoc12: Fancy fancy fix

--- aoc12/src/Main.hs
+++ aoc12/src/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE TupleSections     #-}
 module Main where
 
+import           Data.Fix
 import           Data.Functor
 import           Data.List
 import           Data.Map                   (Map)
@@ -61,12 +62,13 @@ main = readInput >>= challenges
 challenges :: Input -> IO ()
 challenges input = do
   print $ part1 20 input
+  print $ part2 50000000000 input
   return ()
 
 part1 :: Int -> Input -> Int
 part1 its (Input init rules) = sum $ mapMaybe f ii where
   m = rulesToMap rules
-  i = iterate (step m) (0, init)
+  i = iterate step' (0, init)
   f (_, False) = Nothing
   f (i, True)  = Just i
   (startIndex, res) = i !! its
@@ -75,7 +77,7 @@ part1 its (Input init rules) = sum $ mapMaybe f ii where
 part2 :: Int -> Input -> Int
 part2 its (Input init rules) = y where
   m = rulesToMap rules
-  i = iterate (step m) (0, init)
+  i = iterate step' (0, init)
   (index, ((startIndex, res), diff)) = head $ catMaybes $ zipWith (\i m -> (i,) <$> m) [0..] $ zipWith t i (tail i)
   f (_, False) = Nothing
   f (i, True)  = Just i
@@ -90,6 +92,89 @@ t (ithis, this) (inext, next)
 
 rulesToMap :: [Rule] -> Map (P, P, P, P, P) P
 rulesToMap rules = Map.fromList $ map (\(Rule c r) -> (c, r)) rules
+
+newtype Fun a = Fun { unFun :: Bool -> (Bool, a) }
+
+instance Functor Fun where
+  f `fmap` Fun fun = Fun $ \b ->
+    let (bl, a) = fun b in (bl, f a)
+
+type X = Fix Fun
+
+
+tfff :: X
+tfff = Fix $ Fun $ \case
+  False ->  (False, ffff)
+  True -> (True, ffft)
+
+ffff = Fix $ Fun $ \case
+  False -> (False, ffff)
+  True -> (False, ffft)
+
+ffft = Fix $ Fun $ \case
+  False -> (False, fftf)
+  True -> (True, fftt)
+
+fftf = Fix $ Fun $ \case
+  False -> (False, ftff)
+  True -> (True, ftft)
+
+fftt = Fix $ Fun $ \case
+  False -> (False, fttf)
+  True -> (False, fttt)
+
+ftff = Fix $ Fun $ \case
+  False -> (True, tfff)
+  True -> (True, tfft)
+
+ftft = Fix $ Fun $ \case
+  False -> (True, tftf)
+  True -> (True, tftt)
+
+fttf = Fix $ Fun $ \case
+  False -> (False, ttff)
+  True -> (True, ttft)
+
+fttt = Fix $ Fun $ \case
+  False -> (False, tttf)
+  True -> (False, tttt)
+
+tfft = Fix $ Fun $ \case
+  False -> (False, fftf)
+  True -> (False, fftt)
+
+tftf = Fix $ Fun $ \case
+  False -> (True, ftff)
+  True -> (False, ftft)
+
+tftt = Fix $ Fun $ \case
+  False -> (True, fttf)
+  True -> (False, fttt)
+
+ttff = Fix $ Fun $ \case
+  False -> (True, tfff)
+  True -> (True, tfft)
+
+ttft = Fix $ Fun $ \case
+  False -> (True, tftf)
+  True -> (True, tftt)
+
+tttf = Fix $ Fun $ \case
+  False -> (True, ttff)
+  True -> (True, ttft)
+
+tttt = Fix $ Fun $ \case
+  False -> (False, tttf)
+  True -> (True, tttt)
+
+step' :: (Int, [Bool]) -> (Int, [Bool])
+step' (startIndex, tape) = clean (startIndex - 2, result) where
+  extendedTape = tape ++ [False, False, False, False]
+  result = cata cataThing ffff extendedTape
+  cataThing :: Fun ([Bool] -> [Bool]) -> [Bool] -> [Bool]
+  cataThing (Fun fun) [] = []
+  cataThing (Fun fun) (x:xs) = a : b xs where
+    (a, b) = fun x
 
 step :: Map (P, P, P, P, P) P -> (Int, [Bool]) -> (Int, [Bool])
 step rules (startIndex, tape) = clean (startIndex - 2, snd result) where
