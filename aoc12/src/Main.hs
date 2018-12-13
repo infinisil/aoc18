3c7f4c0236138b04f39f8ba5e8602cb424c41e01
Silvan Mosberger <infinisil@icloud.com>
2018-12-13 05:10:30 +0100

    aoc12: Clean up

--- aoc12/src/Main.hs
+++ aoc12/src/Main.hs
@@ -31,38 +31,28 @@ main = readInput >>= challenges
 
 challenges :: Input -> IO ()
 challenges input = do
-  print $ part1 20 input
-  print $ part2 50000000000 input
+  print $ snd $ generation input 20
+  print $ snd $ generation input 50000000000
   return ()
 
-part1 :: Int -> Input -> Int
-part1 its (Input init _) = sum $ mapMaybe f ii where
-  i = iterate step' (0, init)
-  f (_, False) = Nothing
-  f (i, True)  = Just i
-  (startIndex, res) = i !! its
-  ii = zip [startIndex..] res
+generation :: Input -> Int -> ([Bool], Int)
+generation (Input init _) n = findN n detectUnchanging where
+  iterations = iterate step (0, init)
+  detectUnchanging = zipWith unchanged iterations (tail iterations)
+  unchanged (i, this) (j, next)
+    | this == next = ((i, this), Just (j - i))
+    | otherwise = ((i, this), Nothing)
 
-part2 :: Int -> Input -> Int
-part2 its (Input init _) = y where
-  i = iterate step' (0, init)
-  (index, ((startIndex, res), diff)) = head $ catMaybes $ zipWith (\i m -> (i,) <$> m) [0..] $ zipWith t i (tail i)
-  f (_, False) = Nothing
-  f (i, True)  = Just i
-  x = sum $ mapMaybe f $ zip [startIndex..] res
-  count = length $ filter id $ res
-  y = diff * (its - index) * count + x
-
-
-t (ithis, this) (inext, next)
-  | this == next = Just ((ithis, this), inext - ithis)
-  | otherwise = Nothing
+findN :: Int -> [((Int, [Bool]), Maybe Int)] -> ([Bool], Int)
+findN 0 (((startIndex, tape), _):_) = (tape, sum $ zipWith (\i v -> if v then i else 0) [startIndex ..] tape)
+findN n (((startIndex, tape), Just diff):_) = (tape, sum $ zipWith (\i v -> if v then i + n * diff else 0) [startIndex ..] tape)
+findN n ((_, Nothing):rest) = findN (n - 1) rest
 
 rulesToMap :: [Rule] -> Map (P, P, P, P, P) P
 rulesToMap rules = Map.fromList $ map (\(Rule c r) -> (c, r)) rules
 
-step' :: (Int, [Bool]) -> (Int, [Bool])
-step' (startIndex, tape) = clean (startIndex - 2, result) where
+step :: (Int, [Bool]) -> (Int, [Bool])
+step (startIndex, tape) = clean (startIndex - 2, result) where
   extendedTape = tape ++ [False, False, False, False]
   result = cata cataThing ffff extendedTape
   cataThing :: Fun ([Bool] -> [Bool]) -> [Bool] -> [Bool]
