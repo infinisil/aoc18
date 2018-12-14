dc373f73d29e5cd681b23b03a3aee5c1f575c402
Silvan Mosberger <infinisil@icloud.com>
2018-12-14 19:28:29 +0100

    Update template

--- template/src/Main.hs
+++ template/src/Main.hs
@@ -3,6 +3,8 @@ module Main where
 
 import           Data.Text                  (Text)
 import qualified Data.Text.IO               as TIO
+import           Data.Time.Clock
+import           Data.Time.Clock.System
 import           Data.Void
 import           Paths_aoc@day@
 import           System.Directory           (doesFileExist)
@@ -16,6 +18,14 @@ type Input = [Int]
 
 parser :: Parser Input
 parser = many (decimal <* newline) <* eof
+
+time :: IO a -> IO a
+time io = do
+  start <- getSystemTime
+  result <- io
+  stop <- getSystemTime
+  print $ systemToUTCTime stop `diffUTCTime` systemToUTCTime start
+  return result
 
 main :: IO ()
 main = do
